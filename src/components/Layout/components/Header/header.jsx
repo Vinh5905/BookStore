c672944cf396a2d6b134551e5e0ad01c17603de4
EdogawaCoTrung <quangtrungproki@gmail.com>
2024-01-13 20:17:03 +0700

    add

--- src/components/Layout/components/Header/header.jsx
+++ src/components/Layout/components/Header/header.jsx
@@ -4,32 +4,34 @@ import { SlSocialInstagram } from "react-icons/sl";
 import { SlSocialTwitter } from "react-icons/sl";
 function Header() {
     return (
-        <div className="bg-HeaderColor">
-            <nav className="flex justify-between items-center mx-auto w-[92%]">
-                <div className="flex flex-row items-center font-semibold text-white">
-                    <PiPhoneLight className="text-xs sm:text-sm md:text-5xl xl:text-5xl"/>
-                    <p className="text-xs sm:text-sm md:text-5xl xl:text-5xl">+91 8374902234</p>
-                </div>
-                <div>
-                    <ul className="flex items-center gap-6">
-                        <li>
-                            <a className="text-white font-semibold text-xs sm:text-sm md:text-5xl xl:text-5xl hover:text-sky-600" href="#"><TfiFacebook /></a>
-                        </li>
-                        <li>
-                            <a className="text-white font-semibold text-xs sm:text-sm md:text-5xl xl:text-5xl hover:text-pink-400" href="#"><SlSocialInstagram /></a>
-                        </li>
-                        <li>
-                            <a className="text-white font-semibold text-xs sm:text-sm md:text-5xl xl:text-5xl hover:text-sky-200" href="#"><TfiFacebook /></a>
-                        </li>
-                        <li>
-                            <a className="text-white font-semibold text-xs sm:text-sm md:text-5xl xl:text-5xl hover:text-sky-600" href="#"><SlSocialTwitter /></a>
-                        </li>
-                        <li>
-                            <a className="text-white font-semibold text-xs sm:text-sm md:text-5xl xl:text-5xl hover:text-sky-600" href="#"><TfiFacebook /></a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+        <div className="bg-HeaderColor h-14">
+            <div className="py-4">
+                <nav className="flex justify-between items-center mx-auto w-[92%]">
+                    <div className="flex flex-row items-center font-semibold text-white">
+                        <PiPhoneLight className="text-xs sm:text-sm md:text-5xl xl:text-5xl"/>
+                        <p className="text-xs sm:text-sm md:text-5xl xl:text-5xl">+91 8374902234</p>
+                    </div>
+                    <div>
+                        <ul className="flex items-center gap-6">
+                            <li>
+                                <a className="text-white font-semibold text-xs sm:text-sm md:text-5xl xl:text-5xl hover:text-sky-600" href="#"><TfiFacebook /></a>
+                            </li>
+                            <li>
+                                <a className="text-white font-semibold text-xs sm:text-sm md:text-5xl xl:text-5xl hover:text-pink-400" href="#"><SlSocialInstagram /></a>
+                            </li>
+                            <li>
+                                <a className="text-white font-semibold text-xs sm:text-sm md:text-5xl xl:text-5xl hover:text-sky-200" href="#"><TfiFacebook /></a>
+                            </li>
+                            <li>
+                                <a className="text-white font-semibold text-xs sm:text-sm md:text-5xl xl:text-5xl hover:text-sky-600" href="#"><SlSocialTwitter /></a>
+                            </li>
+                            <li>
+                                <a className="text-white font-semibold text-xs sm:text-sm md:text-5xl xl:text-5xl hover:text-sky-600" href="#"><TfiFacebook /></a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
         </div>
     )
 }
