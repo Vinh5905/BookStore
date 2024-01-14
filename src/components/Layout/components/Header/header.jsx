16da085882469e45a6059fabec4bbfb8d7f3745c
EdogawaCoTrung <quangtrungproki@gmail.com>
2024-01-14 14:31:13 +0700

    add

--- src/components/Layout/components/Header/header.jsx
+++ src/components/Layout/components/Header/header.jsx
@@ -2,31 +2,33 @@ import { PiPhoneLight } from "react-icons/pi";
 import { TfiFacebook } from "react-icons/tfi";
 import { SlSocialInstagram } from "react-icons/sl";
 import { SlSocialTwitter } from "react-icons/sl";
+import { ImBlogger } from "react-icons/im";
+import { FaLinkedinIn } from "react-icons/fa6";
 function Header() {
     return (
         <div className="bg-HeaderColor h-14">
             <div className="py-4">
                 <nav className="flex justify-between items-center mx-auto w-[92%]">
                     <div className="flex flex-row items-center font-semibold text-white">
-                        <PiPhoneLight className="text-xs sm:text-sm md:text-5xl xl:text-5xl"/>
-                        <p className="text-xs sm:text-sm md:text-5xl xl:text-5xl">+91 8374902234</p>
+                        <PiPhoneLight className="text-xs sm:text-sm md:text-sm xl:text-base"/>
+                        <p className="text-xs sm:text-sm md:text-sm xl:text-base">+91 8374902234</p>
                     </div>
                     <div>
                         <ul className="flex items-center gap-6">
                             <li>
-                                <a className="text-white font-semibold text-xs sm:text-sm md:text-5xl xl:text-5xl hover:text-sky-600" href="#"><TfiFacebook /></a>
+                                <a className="text-white font-semibold text-xs sm:text-sm md:text-sm xl:text-base hover:text-sky-600" href="#"><TfiFacebook /></a>
                             </li>
                             <li>
-                                <a className="text-white font-semibold text-xs sm:text-sm md:text-5xl xl:text-5xl hover:text-pink-400" href="#"><SlSocialInstagram /></a>
+                                <a className="text-white font-semibold text-xs sm:text-sm md:text-sm xl:text-base hover:text-pink-400" href="#"><SlSocialInstagram /></a>
                             </li>
                             <li>
-                                <a className="text-white font-semibold text-xs sm:text-sm md:text-5xl xl:text-5xl hover:text-sky-200" href="#"><TfiFacebook /></a>
+                                <a className="text-white font-semibold text-xs sm:text-sm md:text-sm xl:text-base hover:text-sky-200" href="#"><FaLinkedinIn /></a>
                             </li>
                             <li>
-                                <a className="text-white font-semibold text-xs sm:text-sm md:text-5xl xl:text-5xl hover:text-sky-600" href="#"><SlSocialTwitter /></a>
+                                <a className="text-white font-semibold text-xs sm:text-sm md:text-sm xl:text-base hover:text-sky-500" href="#"><SlSocialTwitter /></a>
                             </li>
                             <li>
-                                <a className="text-white font-semibold text-xs sm:text-sm md:text-5xl xl:text-5xl hover:text-sky-600" href="#"><TfiFacebook /></a>
+                                <a className="text-white font-semibold text-xs sm:text-sm md:text-sm xl:text-base hover:text-yellow-400" href="#"><ImBlogger /></a>
                             </li>
                         </ul>
                     </div>
