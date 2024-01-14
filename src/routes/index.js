01d61bb56750e011976db554ff5e06dadeae3e44
EdogawaCoTrung <quangtrungproki@gmail.com>
2024-01-14 21:03:06 +0700

    add

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,14 @@ import Register from "../pages/Register/Register";
 import Home from "../pages/Home/Home";
 import { Auth } from "../components/Layout";
 import Header from "../components/Layout/components/Header/header";
+import ProductViewFilter from "../components/Layout/components/ProductViewFilter";
 
 const publicRoutes = [
     { path: '/', component: Home },
     { path: '/login', component: Login, layout: Auth },
     { path: '/register', component: Register, layout: Auth },
-    { path: '/Header', component: Header},
+    { path: '/Header', component: Header },
+    { path: '/ProductViewFilter', component: ProductViewFilter }
 ]
 
 export { publicRoutes }
