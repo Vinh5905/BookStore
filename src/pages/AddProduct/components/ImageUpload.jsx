f40e24d7fab386eb7bf41fbf93bd940802a37f71
Vinh5905 <hoangvinh9257@gmail.com>
2024-01-15 21:03:30 +0700

    Done drop and drag, add author and kind of book input

--- src/pages/AddProduct/components/ImageUpload.jsx
+++ src/pages/AddProduct/components/ImageUpload.jsx
@@ -6,7 +6,6 @@ function ImageUpload() {
   const [images, setImages] = useState([]);
 
   const onChange = (imageList, addUpdateIndex) => {
-    // data for submit
     console.log(imageList, addUpdateIndex);
     setImages(imageList);
   };
@@ -16,12 +15,7 @@ function ImageUpload() {
       <p className="font-medium text-base mb-2 text-black/80 tracking-wide">
         Book's picture
       </p>
-      <ImageUploading
-        value={images}
-        onChange={onChange}
-        // maxNumber={maxNumber}
-        dataURLKey="data_url"
-      >
+      <ImageUploading value={images} onChange={onChange} dataURLKey="data_url">
         {({
           imageList,
           onImageUpload,
@@ -31,15 +25,35 @@ function ImageUpload() {
           isDragging,
           dragProps,
         }) => (
-          <div className="w-full">
-            <div onClick={onImageUpload} {...dragProps} className="bg-orange-50 h-40 border-2 border-[--primary-color] border-dashed rounded-2xl flex items-center justify-center hover:bg-orange-200/30 cursor-pointer">
+          <div className="w-full grid grid-cols-1 justify-center">
+            {imageList.length === 0 ? <div
+              onClick={onImageUpload}
+              {...dragProps}
+              className="w-full bg-orange-50 h-40 border-2 border-[--primary-color] border-dashed rounded-2xl flex items-center justify-center hover:bg-orange-200/30 cursor-pointer"
+            >
               <div className="flex flex-col items-center">
                 <img src={picture} alt="" className="w-16 opacity-50" />
                 <p className="font-light text-black/40">
                   Drag & drop or choose from file
                 </p>
               </div>
-            </div>
+            </div> : imageList.map((image, index) => (
+              <>
+                <div key={index} className="max-h-[32.25rem] w-full flex items-start justify-center p-5 rounded-xl overflow-hidden border-black">
+                  <img
+                    className="rounded-lg h-full w-full object-contain"
+                    src={image["data_url"]}
+                    alt=""
+                  />
+  
+                </div>
+                <div className="w-full flex justify-center">
+                  <button onClick={onImageRemoveAll} className="border border-[--primary-color] text-[--primary-color] px-10 py-2 mb-5 rounded-xl hover:text-white hover:bg-[--primary-color] font-extralight">Remove</button>
+                </div>
+              </>
+
+            ))}
+
           </div>
         )}
       </ImageUploading>
@@ -49,45 +63,3 @@ function ImageUpload() {
 
 export default ImageUpload;
 
-{
-  /* <ImageUploading
-        multiple
-        value={images}
-        onChange={onChange}
-        maxNumber={maxNumber}
-        dataURLKey="data_url"
-      >
-        {({
-          imageList,
-          onImageUpload,
-          onImageRemoveAll,
-          onImageUpdate,
-          onImageRemove,
-          isDragging,
-          dragProps,
-        }) => (
-          // write your building UI
-          <div className="upload__image-wrapper">
-            <button
-              style={isDragging ? { color: 'red' } : undefined}
-              onClick={onImageUpload}
-              {...dragProps}
-            >
-              Click or Drop here
-            </button>
-            &nbsp;
-            <button onClick={onImageRemoveAll}>Remove all images</button>
-            {imageList.map((image, index) => (
-              <div key={index} className="image-item">
-                <img src={image['data_url']} alt="" width="100" />
-                <div className="image-item__btn-wrapper">
-                  <button onClick={() => onImageUpdate(index)}>Update</button>
-                  <button onClick={() => onImageRemove(index)}>Remove</button>
-                </div>
-              </div>
-            ))}
-          </div>
-        )}
-      </ImageUploading>
-    </div> */
-}
